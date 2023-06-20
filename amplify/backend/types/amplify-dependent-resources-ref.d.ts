9ac6288a3a1aad57ebc3177d899857932f7933c7
Saurbh Ranjan Work Mac <saurabhranjan@uber.com>
2023-06-20 14:55:42 +0530

    add amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "imagetag3043fc2e": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "imagetagfe26cb4a": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "s33584b650": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
